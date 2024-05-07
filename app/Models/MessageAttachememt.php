7e8d51356b043fe8fa6987b168c7963d38baf112
Mouad Abou Othmane <mouad0abouothmane@gmail.com>
2024-05-07 20:57:54 +0100

    Generate Models, Migrations, Model fillable and relations

---
+++ app/Models/MessageAttachememt.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MessageAttachememt extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "message_id",
+        "name",
+        "path",
+        "mime",
+        "size",
+    ];
+
+    
+}
