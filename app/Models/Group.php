5693f119535fb498cb594778891cb6474f76cdb2
Mouad Abou Othmane <104225906+MouadAbouOthmane@users.noreply.github.com>
2024-05-31 11:19:18 +0100

    ChatGPT Added Group model with relationships and fillable attributes.

--- app/Models/Group.php
+++ app/Models/Group.php
@@ -16,5 +16,20 @@ class Group extends Model
         'last_message_id',
     ];
 
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'group_users');
+    }
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
 
